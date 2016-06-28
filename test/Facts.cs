9761e6e0631064fdbbc496f2b6b0db627a199f03
Svend Tofte <stofte@gmail.com>
2016-06-28 16:19:18 +0200

    XUnit deps and stub test

---
+++ test/Facts.cs
@@ -0,0 +1,17 @@
+namespace QueryEngine.Test
+{
+    using Xunit;
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}

4ae7275640a3da38321cbde3134c705b4874bb7d
Svend Tofte <stofte@gmail.com>
2016-06-30 16:44:34 +0200

    Fix copy syntax

--- test/ReamQuery.Test/fix-json-config.cmd
+++ test/ReamQuery.Test/fix-json-config.cmd
@@ -10,4 +10,4 @@ cp ../../src/ReamQuery/project.* bin/Debug/netcoreapp1.0/ubuntu.14.04-x64
 exit
 :windows
 copy ..\..\src\ReamQuery\appsettings.* .
-copy ..\..\src\ReamQuery\project.* bin\Debug\netcoreapp1.0\win7-x64
+copy ..\..\src\ReamQuery\project.* bin\Debug\netcoreapp1.0\win7-x64\.

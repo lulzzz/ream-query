01bd8038d8104cd97dd2b4556970d45ef623bb5a
Svend Tofte <stofte@gmail.com>
2016-07-17 21:03:02 +0200

    Assert returned namespace is correct

--- query/test/ReamQuery.Test/QueryTemplateEndpoint.cs
+++ query/test/ReamQuery.Test/QueryTemplateEndpoint.cs
@@ -28,9 +28,12 @@ namespace ReamQuery.Test
             var res = await _client.PostAsync(EndpointAddress, new StringContent(json));
             var jsonRes = await res.Content.ReadAsStringAsync();
             var output = JsonConvert.DeserializeObject<TemplateResponse>(jsonRes);
+            var ns = output.Namespace;
             var nodes = CSharpSyntaxTree.ParseText(output.Template).GetRoot().DescendantNodes();
             var tbls = nodes.OfType<ClassDeclarationSyntax>();
+
             Assert.Equal(StatusCode.Ok, output.Code);
+            Assert.NotEmpty(nodes.OfType<NamespaceDeclarationSyntax>().Where(x => x.Name.ToString() == output.Namespace));
             Assert.Single(tbls.Where(tbl => tbl.Identifier.ToString() == "city"));
             Assert.Single(tbls.Where(tbl => tbl.Identifier.ToString() == "country"));
             Assert.Single(tbls.Where(tbl => tbl.Identifier.ToString() == "countrylanguage"));
